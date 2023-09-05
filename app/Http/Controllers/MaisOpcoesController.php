cc6a3c63527c6c2638b29b62718973ad2097e435
kerrymuniz <kerrym877@gmail.com>
2023-09-05 17:50:11 -0300

    comitação normal

---
+++ app/Http/Controllers/MaisOpcoesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MaisOpcoesController extends Controller
+{
+    public function maisopcoes() {
+        echo 'Mais opções...';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,39 @@ use Illuminate\Support\Facades\Route;
 */
 
 /*Route::get('/', function () {
-    return 'Olá seja bem vindo ao curso';
+    return 'Olá, seja bem-vindo ao curso!';
 });*/
 
 Route::get('/', 'PrincipalController@principal');
 
-Route::get('/sobrenos', 'SobreNosController@sobreNos');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
+
+
+
+    
+
+
+
+/* tratando parâmetros com expressões regulares 
+
+Route::get('/contato/{nome}/{categoria_id}', function(string $nome = 'Desconhecido', int $categoria_id = 1 /* 1 = 'Informação') {
+    echo "Estamos aqui: $nome - $categoria_id ";
+})->where('categoria_id', '[0-9]+') //tratando os parâmetros, recebendo primeiro qual o parâmetro a ser tradado, e depois a condição (nesse caso está dizendo que deve estar entre 0 e 9 e pelo menos 1 caracter enviado (+));
+->where('nome', '[A-Za-z]+'); //tratando os parâmetros, recebendo primeiro qual o parâmetro a ser tradado, e depois a condição (nesse caso está dizendo que deve estar entre A-Z e a-z e pelo menos 1 caracter enviado (+));
+
+*/
+
+
+/* enviando parâmetro para a rota
+    + parâmetro opcional (?)
+
+Route::get('/contato/{nome}/{cat}/{ass}/{msg?}', function(string $nome, string $cat, string $ass, string $msg = 'nao informada') {
+echo "Estamos aqui $nome - $cat - $ass - $msg";
+});
+
+*/
 
 /* verbo http
 
