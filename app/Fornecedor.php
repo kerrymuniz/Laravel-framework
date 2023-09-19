2c2d3baef1c606f061108d5aca5b898f26fb9fb8
kerrymuniz <kerrym877@gmail.com>
2023-09-19 17:50:28 -0300

    comitacao normal

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+//fornecedors
+//fornecedores
+
 class Fornecedor extends Model
 {
-    //
+    //correção do nome da tabela, pois como a classe se chama Fornecedor, ao acrescentar o 's' no bd, irá ocasionar um erro;
+    protected $table = 'fornecedores';
+    //autorizando os registros que irão ser inseridos no tinker a partir do método create();
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
