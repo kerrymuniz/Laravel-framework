40be819c66776c7e067f8fce9fca2b82d367f50b
kerrymuniz <kerrym877@gmail.com>
2023-09-21 17:55:52 -0300

    alteracao normal

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,12 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 //fornecedors
 //fornecedores
 
 class Fornecedor extends Model
 {
+    //
+    use SoftDeletes;
+
     //correção do nome da tabela, pois como a classe se chama Fornecedor, ao acrescentar o 's' no bd, irá ocasionar um erro;
     protected $table = 'fornecedores';
     //autorizando os registros que irão ser inseridos no tinker a partir do método create();
---
+++ database/seeds/FornecedorSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Fornecedor;
+
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Instanciando o obbjeto ( criando registros no seeder );
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'CE';
+        $fornecedor->email = 'contato@fornecedor100.com.br';
+        $fornecedor->save();
+
+        /* os três tem a mesma finalidade, só são meios diferentes de inserir registros no seeder */
+
+        //Método create ( atenção para o atributo fillable da classe );
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'fornecedor200.com.br',
+            'uf' => 'RS',
+            'email' => 'contato@fornecedor200.com.br'
+        ]);
+
+        //Insert;
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'fornecedor300.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor300.com.br'
+        ]);
+    }
+}
