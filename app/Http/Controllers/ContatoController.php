847c3e36e6849090d956ed8cfc9287c29984b369
kerrymuniz <kerrym877@gmail.com>
2023-09-25 18:00:00 -0300

    alteracao normal

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -17,15 +17,39 @@ class ContatoController extends Controller
         echo $request->input('email');
         */
 
-        $contato = new SiteContato();
+        /*$contato = new SiteContato(); //instância da classe SiteContato() do model SiteContato;
         $contato->nome = $request->input('nome');
         $contato->telefone = $request->input('telefone');
         $contato->email = $request->input('email');
         $contato->motivo_contato = $request->input('motivo_contato');
         $contato->mensagem = $request->input('mensagem');
+        
 
         //print_r($contato->getAttributes());
-        $contato->save();
-        return view('site.contato');
+        $contato->save();*/
+
+        //outra forma de salvar os dados do formulário no banco de dados;
+        //$contato->create($request->all()); //criando uma $request para assim salvar os dados do formulário no banco;
+
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
+    }
+
+    public function salvar(Request $request) {
+
+        //realiziar a valdação dos dados do formulário recebidos no request
+        $request->validate([ //para isso se usa o método validate([]) recebenndo como parmt array assoc com os campos a serem validados;
+            //o required é um dos tipos de validação, isso significa que esse campo deve ser obrigatoriamente preenchido;
+            'nome' => 'required|min:3|max:40', //nomes com no mínimo 3 caracteres e no máximo 40;
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000' //mensagem pode ter no máximo 2000 caracteres;
+        ]);
+        //SiteContato::create($request->all());
     }
 }
--- app/SiteContato.php
+++ app/SiteContato.php
@@ -10,5 +10,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
-    //
+    //criando o fillable e dizendo quais atributos vao ser salvos no banco de dados após enviar o formuláro;
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
 }
